611d346c74216ad3931f367fc6cd86431aabcb6a
Radhi Bajahaw <rathi7137@gmail.com>
2026-03-18 11:20:04 +0100

    yet another reasoning field

--- streamaccumulator.go
+++ streamaccumulator.go
@@ -121,6 +121,7 @@ func (cc *ChatCompletion) accumulateDelta(chunk ChatCompletionChunk) bool {
 
 		choice.Message.Reasoning += delta.Delta.Reasoning
 		choice.Message.ReasoningText += delta.Delta.ReasoningText
+		choice.Message.ReasoningContent += delta.Delta.ReasoningContent
 		choice.Message.Content += delta.Delta.Content
 		choice.Message.Refusal += delta.Delta.Refusal
 
